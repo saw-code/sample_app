0b790922797f8ce0271945b528ce213924285280
saw-code <saw.coding@gmail.com>
2021-09-06 21:42:48 +0300

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'user/new'
   root 'static_pages#home'
+  get  'users/new' #нужен ли???
+  # Этот новый шаблон направляет запрос GET для URL/help к методу help в контроллере StaticPages. Как и в случае с правилом для корневого маршрута, это создает два именованных маршрута, help_path и help_url:
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,6 @@ require "rails/test_help"
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
