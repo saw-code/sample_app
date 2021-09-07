26263bd9d3437f8623ede8999333659de86cdcdf
saw-code <saw.coding@gmail.com>
2021-09-07 20:32:57 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id]) # мы используем метод find в модели User для извлечения пользователя из БД
+    # Когда мы делаем соответствующий запрос к контроллеру Users, params[:id] получит значение 1.
+    # Технически params[:id] - это строка "1"
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'users/new' #нужен ли???
   # Этот новый шаблон направляет запрос GET для URL/help к методу help в контроллере StaticPages. Как и в случае с правилом для корневого маршрута, это создает два именованных маршрута, help_path и help_url:
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  resources :users
 end
