bb0b554281cdb926592b0a74c43717cba1b5e189
saw-code <saw.coding@gmail.com>
2021-09-08 17:37:51 +0300

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   # Этот новый шаблон направляет запрос GET для URL/help к методу help в контроллере StaticPages. Как и в случае с правилом для корневого маршрута, это создает два именованных маршрута, help_path и help_url:
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
