9c73cfa8ed40f3e875c57a23a8a4e9c7e268204d
saw-code <saw.coding@gmail.com>
2021-09-10 14:23:41 +0300

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
   # Этот новый шаблон направляет запрос GET для URL/help к методу help в контроллере StaticPages. Как и в случае с правилом для корневого маршрута, это создает два именованных маршрута, help_path и help_url:
@@ -10,4 +12,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
